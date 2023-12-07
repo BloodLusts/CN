da6fdcca720587a71cebbfb79750f5fc7ac09ea8
BloodLusts <130475524+BloodLusts@users.noreply.github.com>
2023-12-07 22:45:51 +0530

    changes

--- swapNum.c
+++ swapNum.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main(){
     int x=3,y=5;
+    printf("Before swapping x=%d and y=%d",x,y);
     x=x+y;
     y=x-y;
     x=x-y;
+    printf("After swapping");
     printf("x=%d\n",x);
     printf("y=%d\n",y);
 
